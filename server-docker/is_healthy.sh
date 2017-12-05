0aa719f4716e3846f739ba59e1fa6a092e00e3d4
shawkins <shawkins@redhat.com>
2017-12-05 15:18:35 -0500

    template/image updates:
    adding pg/mysql modules
    adding image change trigger
    updating health check

--- server-docker/is_healthy.sh
+++ server-docker/is_healthy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-JAVA_OPTS=-Xmx16m /opt/jboss/teiid-server/bin/jboss-cli.sh -c --controller=$(hostname -i):9990 "/subsystem=teiid:get-vdb(vdb-name=$1, vdb-version=$2)" | grep "\"status\" => \"ACTIVE\""
+JAVA_OPTS=-Xmx16m /opt/jboss/teiid-server/bin/jboss-cli.sh -c --controller=$(hostname -i):9990 "/subsystem=teiid/:execute-query(sql-query=\"select 1\", vdb-name=$1, vdb-version=$2, timeout-in-milli=100)" | grep "\"outcome\" => \"success\""
 
 exit $?
