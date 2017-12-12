34aa37ed69a8804fc873237768784ddcb8615fc7
shawkins <shawkins@redhat.com>
2017-12-12 18:18:56 -0500

    TEIID-5131 adding more meta templating for mysql/pg

--- templates/create-template.sh
+++ templates/create-template.sh
@@ -7,6 +7,7 @@ while getopts "t:" opt;
 do
 	case $opt in
 	t) TYPE=$OPTARG ;;
+	n) TYPE_NAME=$OPTARG ;;
 	h) HA="-ha" ;;
 	esac
 done
@@ -16,11 +17,28 @@ if [ -z "$TYPE" ]; then
   exit 1
 fi
 
-# inject ra.xml
-sed -e "s/\${resource-adapter-xml}/$(<$TYPE/$TYPE-ra.xml sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" standalone-teiid${ha}.xml > standalone-teiid.tmp
+if [ -z "$TYPE_NAME" ]; then
+  TYPE_NAME=$TYPE
+fi
+
+# inject ra.xml or ds.xml
+if [ -f "$TYPE/$TYPE-ra.xml" ]; then
+  sed -e "s/<!--resource-adapter-xml-->/$(<$TYPE/$TYPE-ra.xml sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" standalone-teiid${ha}.xml > standalone-teiid.tmp
+else
+  sed -e "s/<!--datasource-xml-->/$(<$TYPE/$TYPE-ds.xml sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" standalone-teiid${ha}.xml > standalone-teiid.tmp
+fi
+
 # remove new lines
 sed -e ':a;N;$!ba;s/\n/\\n/g' standalone-teiid.tmp > standalone-teiid-1.tmp
 sed -e ':a;N;$!ba;s/\n/\\n/g' $TYPE/$TYPE-vdb.xml > vdb.tmp
 # inject vdb
-sed -e "s/\${vdb-xml}/$(<vdb.tmp sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" -e "s/\${config-xml}/$(<standalone-teiid-1.tmp sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" $TYPE/teiid-$TYPE-template.json > teiid-$TYPE-template.json
+
+#select or create the base template
+TEMPLATE_FILE=$TYPE/teiid-$TYPE-template.json
+if [ ! -f "$TEMPLATE_FILE" ]; then
+  sed -e "s/#name#/$TYPE/g" -e "s/#full-name#/$TYPE_NAME/g" teiid-template.json > teiid-$TYPE-template.tmp
+  TEMPLATE_FILE=teiid-$TYPE-template.tmp
+fi
+
+sed -e "s/\${vdb-xml}/$(<vdb.tmp sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" -e "s/\${config-xml}/$(<standalone-teiid-1.tmp sed -e 's/[\&/]/\\&/g' -e 's/$//' | tr -d '\n')/g" $TEMPLATE_FILE > teiid-$TYPE-template.json
 rm *.tmp
